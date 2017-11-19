e8446a180fa91913ff11a263d6b294b82278c9e3
chomtana <chomtana001@gmail.com>
2017-11-19 15:19:50 +0000

    Create 1

--- js/shared.js
+++ js/shared.js
@@ -113,7 +113,7 @@ $(window).on('popstate', function (e) {
 });
 
 $(document).ready(function() {
-  $("a").click(function(e) {
+  $("a").not(".normal-a").click(function(e) {
     e.preventDefault();
     changepage($(this).attr("href"));
   });
@@ -132,3 +132,13 @@ $(document).ready(function() {
     if ($(this).find("li").html()=="EduT-") $(this).remove();
   });
 });
+
+
+//Google analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-104199832-1', 'auto');
+ga('send', 'pageview');
