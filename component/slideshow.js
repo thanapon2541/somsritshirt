48fe1097542bdfa8c73c07558c4889cb12a5d1df
chomtana <chomtana001@gmail.com>
2017-12-15 16:44:50 +0000

    old mobile fix

--- component/slideshow.js
+++ component/slideshow.js
@@ -1,66 +1,32 @@
 Vue.component('slideshow',{
     template: `
-        <!-- Container for the image gallery -->
+    <div class="slideshow-vue-wrap">
+        <!-- Slideshow container -->
         <div class="slideshow-container">
-            <!-- Full-width images with number text -->
-            <div class="mySlides">
-                <div class="numbertext">1 / 6</div>
-                <img src="img_woods_wide.jpg" style="width:100%">
-            </div>
-            <div class="mySlides">
-                <div class="numbertext">2 / 6</div>
-                <img src="img_fjords_wide.jpg" style="width:100%">
-            </div>
-            <div class="mySlides">
-                <div class="numbertext">3 / 6</div>
-                <img src="img_mountains_wide.jpg" style="width:100%">
-            </div>
-            <div class="mySlides">
-                <div class="numbertext">4 / 6</div>
-                <img src="img_lights_wide.jpg" style="width:100%">
-            </div>
-            <div class="mySlides">
-                <div class="numbertext">5 / 6</div>
-                <img src="img_nature_wide.jpg" style="width:100%">
-            </div>
-            <div class="mySlides">
-                <div class="numbertext">6 / 6</div>
-                <img src="img_snow_wide.jpg" style="width:100%">
+            <!-- Full-width images with number and caption text -->
+            <div v-for="(img,i) in data" class="mySlides" :style="{'display':(current==i?'block':'none')}">
+                <div class="numbertext">{{i+1}} / {{data.length}}</div>
+                <img :src="img" :style="{'width':'100%'}">
             </div>
             <!-- Next and previous buttons -->
-            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-            <a class="next" onclick="plusSlides(1)">&#10095;</a>
-            <!-- Image text -->
-            <div class="caption-container">
-                <p id="caption"></p>
-            </div>
-            <!-- Thumbnail images -->
-            <div class="row">
-                <div class="column">
-                    <img class="demo cursor" src="img_woods.jpg" style="width:100%" onclick="currentSlide(1)" alt="The Woods">
-                </div>
-                <div class="column"> 
-                    <img class="demo cursor" src="img_fjords.jpg" style="width:100%" onclick="currentSlide(2)" alt="Trolltunga, Norway">
-                </div>
-                <div class="column">
-                    <img class="demo cursor" src="img_mountains.jpg" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
-                </div>
-                <div class="column">
-                    <img class="demo cursor" src="img_lights.jpg" style="width:100%" onclick="currentSlide(4)" alt="Northern Lights">
-                </div>
-                <div class="column">
-                    <img class="demo cursor" src="img_nature.jpg" style="width:100%" onclick="currentSlide(5)" alt="Nature and sunrise">
-                </div>
-                <div class="column">
-                    <img class="demo cursor" src="img_snow.jpg" style="width:100%" onclick="currentSlide(6)" alt="Snowy Mountains">
-                </div>
-            </div>
+            <a class="prev" v-on:click="current--; if(current<0) current=0;">&#10094;</a>
+            <a class="next" v-on:click="current++; if(current>=data.length) current=data.length-1;">&#10095;</a>
         </div>
+        
+        <!-- The dots/circles -->
+        <div style="text-align:center">
+            <span v-for="(img,i) in data" class="dot" v-on:click="current=i"></span> 
+        </div>
+    </div>
     `,
-    props: ['data','offset'],
+    props: ['data'],
+    data: function() {
+        return {
+            current: 0
+        };
+    },
     created: function() {
-        if (!this.offset) this.offset = 0;
-        console.log("dsdadas");
+        
     },
     methods: {
         
--- js/shared.js
+++ js/shared.js
@@ -1,40 +1,44 @@
 //Old browser not supported
+var isoldandroid = false;
 $(document).ready(function() {
-  function getAndroidVersion(ua) {
-      ua = (ua || navigator.userAgent).toLowerCase(); 
-      var match = ua.match(/android\s([0-9\.]*)/);
-      return match ? match[1] : false;
-  };
-  function getChromeVersion () {     
-      var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
-  
-      return raw ? parseInt(raw[2], 10) : false;
-  }
-  var nua = navigator.userAgent;
-  var isoldandroid = ((nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1) && (!(nua.indexOf('Chrome') > -1) || parseFloat(getAndroidVersion(nua))<5));
-  if (nua.indexOf('Chrome') > -1 && getChromeVersion()>=40) {
-    isoldandroid = false;
-  }
-  if (isoldandroid) {
-    alert("Browser ของคุณอาจไม่รองรับการแสดงผลหน้าเว็บ แนะนำให้คุณเปลี่ยนไปใช้ Google Chrome เวอร์ชั่นล่าสุด");
-  }
-  //alert(navigator.userAgent);
+    function getAndroidVersion(ua) {
+        ua = (ua || navigator.userAgent).toLowerCase(); 
+        var match = ua.match(/android\s([0-9\.]*)/);
+        return match ? match[1] : false;
+    };
+    function getChromeVersion () {     
+        var raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+    
+        return raw ? parseInt(raw[2], 10) : false;
+    }
+    var nua = navigator.userAgent;
+    isoldandroid = ((nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1) && (!(nua.indexOf('Chrome') > -1) || parseFloat(getAndroidVersion(nua))<5));
+    if (nua.indexOf('Chrome') > -1 && getChromeVersion()>=40) {
+        isoldandroid = false;
+    }
+    
+    if (isoldandroid) {
+        //alert("Browser ของคุณอาจไม่รองรับการแสดงผลหน้าเว็บ แนะนำให้คุณเปลี่ยนไปใช้ Google Chrome เวอร์ชั่นล่าสุด");
+    }
+    
+    //alert("duck");
+    //alert(navigator.userAgent);
 });
 
 // Scroll top 
 $(document).on('click','.scroll_top',function(){
-  $('body,html').animate({scrollTop:0},400);
-  return false;
+    $('body,html').animate({scrollTop:0},400);
+    return false;
 });
 
 setInterval(function() {
-  if ($(window).scrollTop()==0) {
-    $(".scroll_top").css("pointer-events","none");
-    $(".scroll_top").css("opacity",0);
-  } else {
-    $(".scroll_top").css("pointer-events","auto");
-    $(".scroll_top").css("opacity",1);
-  }
+    if ($(window).scrollTop()==0) {
+      $(".scroll_top").css("pointer-events","none");
+      $(".scroll_top").css("opacity",0);
+    } else {
+      $(".scroll_top").css("pointer-events","auto");
+      $(".scroll_top").css("opacity",1);
+    }
 },200);
 
 //fade loading transition use pace.js instead (update fade.css to use pace class selector instead of writing new fucking long js)
@@ -47,3 +51,11 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-104199832-1', 'auto');
 ga('send', 'pageview');
+
+//mobile fix
+//fix menu
+$(document).ready(function() {
+    if(isoldandroid) {
+        $("body").append('<div class="menu-wapp clearfix"><div class="wappp"><div class="menum"><div class="container"> <aside class="sidebar"> <nav class="nav"> <ul class="nav-items"> <li class="nav-title">เมนู</li><li> <a href="shop.html" class="nav-link">สั่งทำเสื้อ</a> </li><li> <a href="Blogs.html" class="nav-link">บล็อก</a> </li><li> <a href="contact.html" class="nav-link">ติดต่อสมศรี</a> </li></ul> </nav> </aside> <div class="hamburger"> <div class="bar"></div><div class="bar"></div><div class="bar"></div></div></div><script src="js/index_tgm.js"></script></div><div class="logoim left"><a href="index.html"><img class="loim" src="im/somlogo.png"></a></div><nav class="menudis right"><ul class="meundis_ul"><div class="dr"><a href="shop.html"><li class="meundis_li">สั่งทำเสื้อ</li></a></div><div class="dr"><a href="Blogs.html"><li class="meundis_li">บล็อก</li></a></div><a href="contact.html"><li class="meundis_li meundis_linone">ติดต่อสมศรี</li></a></ul></nav></div></div>');
+    }
+});
